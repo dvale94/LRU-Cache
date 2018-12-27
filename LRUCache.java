84c0c3bd20c5ed357e075fe252f1dad448391daf
Daniel Valencia <valenciad1994@gmail.com>
2018-12-27 17:46:29 -0500

    added node class and node handlers for the doubly linked list

--- LRUCache.java
+++ LRUCache.java
@@ -1,21 +1,67 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lrucache;
+import java.util.HashMap;
 
 /**
  *
- * @author SteamierSix
+ * @author Daniel Valencia
  */
+
+/* 
+ * Class for the nodes of the doubly linked list.
+ */
+class Node {
+	int value;
+	int key;
+	Node left;
+	Node right;
+}
+
 public class LRUCache {
 
-    /**
-     * @param args the command line arguments
+    HashMap<Integer, Node> map;
+    Node head;
+    Node tail;
+    final int LRU_SIZE = 4;
+    
+    /* Constructor for the LRUCache. For the implementation, the LRU cache size  
+     * is 4, but it can be made dynamic.
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public LRUCache() {
+        map = new HashMap<Integer, Node>();
     }
     
+    
+    /* 
+     * Sets a node as the most recently accessed
+     */
+    public void addToHead(Node node) {
+        node.right = head;
+	node.left = null;
+	if (head != null) {
+            head.left = node;
+        }
+        head = node;
+	if (tail == null) {
+            tail = head;
+        }    
+    }
+    
+    /* Removes a node from the queue and sets the values for previous node 
+     * and next node of the surrounding nodes accordingly 
+     */
+    public void removeNode(Node node) {
+        if (node.left != null) {
+            node.left.right = node.right;
+	} 
+        else {
+            head = node.right;
+	}
+
+	if (node.right != null) {
+            node.right.left = node.left;
+	} 
+        else {
+            tail = node.left;
+	}
+    }   
 }
