5594b7f789c98cd44faf856564a0ef7230691d05
Daniel Valencia <valenciad1994@gmail.com>
2019-01-02 13:51:53 -0500

    added methods to insert an element to queue and to get element value from queue

--- LRUCache.java
+++ LRUCache.java
@@ -63,5 +63,52 @@ public class LRUCache {
         else {
             tail = node.left;
 	}
-    }   
+    }  
+    
+    /* 
+     * Inserts the given element into the queue.
+     * If the key exist, update the value and move it to head of queue
+     * If max queue size is reached, make room for new element.
+     */
+    public void putEntry(int key, int value) {
+	if (map.containsKey(key)) {
+            Node entry = map.get(key);
+            entry.value = value;
+            removeNode(entry);
+            addToHead(entry);
+	} 
+        else {
+            Node newEntry = new Node();
+            newEntry.left = null;
+            newEntry.right = null;
+            newEntry.value = value;
+            newEntry.key = key;
+            
+            if (map.size() > LRU_SIZE) {
+		map.remove(tail.key);
+		removeNode(tail);				
+		addToHead(newEntry);
+            } 
+            else {
+		addToHead(newEntry);
+            }
+
+            map.put(key, newEntry);
+	}
+}
+    
+    /* 
+     * References hasmap to return the value of the node with the given key.
+     * Sets the returned node as the most recently accessed.
+     * If the node does not eist, returns -1.
+     */
+    public int get(int key) {
+        if (map.containsKey(key)) {
+            Node entry = map.get(key);
+            removeNode(entry);
+            addToHead(entry);
+            return entry.value;
+	}
+	return -1;
+    }
 }
